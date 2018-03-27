4242ec9c75277e0390e363a6a327ae187896d9ad
shauntc <shaun@shauntc.com>
2018-03-26 17:09:01 -0700

    Initial Commit

---
+++ W5D1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  W5D1
+//
+//  Created by uForis VR on 2018-03-26.
+//  Copyright © 2018 shauntc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
